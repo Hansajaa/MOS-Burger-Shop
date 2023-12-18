876a14968a338d7f088a206060bfdb64d056e3d9
Hansajaa <nilanahansaja21@gmail.com>
2023-12-18 12:17:27 +0530

    adding customer table row

--- frontend Implementation/js/customer.js
+++ frontend Implementation/js/customer.js
@@ -1,18 +1,54 @@
 
+// customer list
 const customerList = []
 
+// customer table
+const customerTable = document.getElementById("tblCustomer");
+
+// table body
+const tableBody = document.getElementById('tblBody');
+
+// add button action
 document.getElementById("btnCustAdd").onclick = function(){
-        
+    
+    let custContactNumber = document.getElementById("custContactNumber").value;
+    let custName = document.getElementById("custName").value;
+
     customerList.push(
         {
-            contactNumber : document.getElementById("custContactNumber").value,
-            Name : document.getElementById("custName").value
+            contactNumber : custContactNumber,
+            Name : custName
         }
     );
 
-    document.getElementById("custContactNumber").value = ''
-    document.getElementById("custName").value = ''
+    const newCustomer = createCustomer(custContactNumber, custName);
+    tableBody.appendChild(newCustomer);
+
+    document.getElementById("custContactNumber").value = '';
+    document.getElementById("custName").value = '';
 
     console.log(customerList);
+
+    
 }
 
+// create new customer row
+function createCustomer(number, name){
+    const row = document.createElement('tr');
+    
+    const cNumber = document.createElement('td');
+    cNumber.textContent=number;
+    row.appendChild(cNumber);
+
+    const cName = document.createElement('td');
+    cName.textContent = name;
+    row.appendChild(cName);
+
+    const btn = document.createElement('td');
+    const btnDelete = document.createElement('button');
+    btnDelete.textContent = "Delete";
+    btn.appendChild(btnDelete);
+    row.appendChild(btn);
+
+    return row;
+}
